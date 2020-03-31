1728c6e20ec0c2fdeb180e30fe38228c4c57a8b5
Clayton Lourenço <clayton@ellevo.com.br>
2020-03-30 21:03:50 -0300

    status

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cd /var/www/scrumonline
 /usr/bin/php ./bin/composer install --no-plugins --no-scripts
 cp src/sample-config.php src/config.php
 # Overwrite host
-echo '$host = "http://localhost:8080";' >> src/config.php
+echo '$host = "http://scrumellevo.southcentralus.cloudapp.azure.com";' >> src/config.php
